4675c6016eab8bb6fa6e0e487fa486cea7aa96d0
chiefanunzio <chiefa.nunzio@gmail.com>
2021-04-07 22:29:30 +0200

    ??

--- js/main.js
+++ js/main.js
@@ -1,9 +1,55 @@
-
-
 function carousel() {
 
+   new Vue({
+
+      el: '#box',
+
+      data: {
+
+         'visible': 0,
+         'images': [
+
+            "img/img1.jpg",
+            "img/img2.jpg",
+            "img/img3.jpg",
+            "img/img4.jpg",
+            "img/img5.jpg",
+            "img/img6.jpg"
+
+         ]
+
+      },
+      methods: {
+
+         prevImg: function () {
+
+            let count = this.visible;
+            let limitCount = (this.images.length - 1);
+
+            if (count == limitCount) {
+               count = 0;
+            } else {
+               count--;
+            }
 
 
+         },
+
+         nextImg: function () {
+            let count = this.visible;
+            let limitCount = (this.images.length - 1);
+
+            if (count == limitCount) {
+               count = 0;
+            } else {
+               count++;
+            }
+
+
+         }
+      }
+
+   });
 }
 
 
@@ -11,7 +57,7 @@ function carousel() {
 
 function init() {
 
-   const start = carousel();
+   carousel();
 
 
 }
